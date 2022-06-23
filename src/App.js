ca63cb10b7f5fcbba0fc741e37a34d270ac29f82
ishigami-prart <ishigami.takatoshi@prart.jp>
2022-06-24 08:16:54 +0900

    axsiosの使い方

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import "./styles.css";
 
 export default function App() {
   const onClickUsers = () => {
-    axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
-      console.log(res);
-    });
+    axios
+      .get("https://jsonplaceholder.typicode.com/users/userxxx")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const onClickUser1 = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users?id=1")
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
   };
 
-  const onClickUser1 = () => {
-    alert();
-  };
   return (
     <div className="App">
       <button onClick={onClickUsers}>users</button>
